2a371e5bf5e37d5e53e3597f95210b5b0d7e8268
Andrew <finnann03@gmail.com>
2026-03-12 18:35:36 +0700

    Add files via upload

---
+++ C++/InternetTrash/Модифиционованный перевод числа из метров в километры.cpp
@@ -0,0 +1,39 @@
+﻿// http://cppstudio.com/cat/285/286/ <- Задание от сюда
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int f;
+	float a, b;
+
+	cout << "МОДИФИЦИРОВАННЫЙ ВЕЛИКИЙ ПЕРЕВОДЧИК МЕТРОВ В КИЛОМЕТРЫ" << endl << endl;
+
+	cout << "Выберите, что переводите - километры в метры или метры в километры" << endl;
+
+	cout << "1. Метры" << endl;
+	cout << "2. Километры" << endl;
+
+	cin >> f;
+
+	if (f == 1) {
+		cout << "Введите метры: "; cin >> a;
+		cout << endl;
+		b = a / 1000;
+
+		cout << a << " метров будет " << b << " километров." << endl;
+	}
+	else if (f == 2) {
+		cout << "Введите километры: "; cin >> a;
+		cout << endl;
+		b = a * 1000;
+
+		cout << a << " километров будет " << b << " метров." << endl;
+	}
+	else {
+		cout << "Ну всё ты сломал код, молодец";
+	}
+	return 0;
+}
---
+++ C++/InternetTrash/Перевод числа из метров в километры.cpp
@@ -0,0 +1,17 @@
+﻿// http://cppstudio.com/cat/285/286/ <- Задание от сюда
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	float a, b;
+
+	cout << "ВЕЛИКИЙ ПЕРЕВОДЧИК МЕТРОВ В КИЛОМЕТРЫ" << endl << endl;
+	cout << "Введите метры: "; cin >> a;
+	cout << endl;
+	b = a / 1000;
+
+	cout << a << " метров будет " << b << " километров." << endl;
+}
