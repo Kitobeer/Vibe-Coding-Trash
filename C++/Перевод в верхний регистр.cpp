03297b3dee196ad89e90a16a4152b9e1cb7c7a50
Andrew <finnann03@gmail.com>
2026-03-12 18:09:55 +0700

    Add files via upload

---
+++ C++/Перевод в верхний регистр.cpp
@@ -0,0 +1,15 @@
+﻿// http://cppstudio.com/cat/285/286/ <- Задание от сюда
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	string stroka;
+
+	cout << "Введите букву нижнего регистра (латинскую): " << endl;	
+	cin >> stroka;
+	cout << "Буква верхнего регистра: " << char(stroka[0] - 32) << endl;
+}
