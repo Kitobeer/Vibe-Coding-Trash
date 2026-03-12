4b2f0b62593157906e46f5fde5c7f499c4fa5a48
Andrew <finnann03@gmail.com>
2026-03-12 17:52:42 +0700

    Add files via upload

---
+++ C++/Деление числа на разряды.cpp
@@ -0,0 +1,28 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a = 0;
+	cout << "Введите пятизначное число: " << endl;
+	cin >> a;
+	if (a < 10000 || a > 99999) {
+		cout << "Вы ввели не пятизначное число!" << endl;
+		return 0;
+	}
+
+	cout << "Первая цифра равна: " << a / 10000 << endl;
+	cout << "Вторая цифра равна: " << (a / 1000) % 10 << endl;
+	cout << "Третья цифра равна: " << (a / 100) % 10 << endl;
+	cout << "Четвертая цифра равна: " << (a / 10) % 10 << endl;
+	cout << "Пятая цифра равна: " << a % 10 << endl;
+
+	return 0;
+}
---
+++ C++/Запрограммировать математическое выражение в С++.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	int a, b, c, x;
+
+	cout << "Введите цифры a, b и c: " << endl;
+	cout << "a: "; cin >> a;
+	cout << "b: "; cin >> b;
+	cout << "c: "; cin >> c;
+
+	cout << "Вычисляем по формуле: x = (a + b - c / a) + c * a * a - (a + b)" << endl;
+
+	x = (a + b - c / a) + c * a * a - (a + b);
+
+	cout << "Результат: x = " << x << endl;
+
+	return 0;
+}
